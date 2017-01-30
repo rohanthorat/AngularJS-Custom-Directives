6b41470ac899984d4beecbd618850ea7b8d861fc
Rohan Thorat <rthorat1@binghamton.edu>
2017-01-29 20:15:12 -0500

    Intial repo

---
+++ isolateScopeWithStringDirective.js
@@ -0,0 +1,16 @@
+(function() {
+
+  var app = angular.module('directivesModule');
+
+  app.directive('isolateScopeWithString', function () {
+      return {
+
+          scope:{
+
+              name: '@'
+          },
+          template: 'Name: {{name}}'
+      };
+  });
+
+}());
